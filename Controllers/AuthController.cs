a5c0cf84dd8d5a63e7642fbad5afe39df569b966
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2026-03-05 21:36:51 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using EduFlow.Data;
+using EduFlow.DTOs;
+using EduFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduFlow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if(_context.Users.Any(u => u.Email == dto.Email))
+            {
+                return BadRequest("Email already exists");
+            }
+
+            var user = new User
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = "Student",
+                CreatedAt = DateTime.UtcNow
+            };
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("User registered succesfully");
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EduFlow.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduFlow.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+        }
+        public DbSet<User> Users { get; set;}
+    }
+}
