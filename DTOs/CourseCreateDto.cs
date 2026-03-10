4480820a6e1bcc7dc7c26e7989a9eefa1acbf131
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2026-03-10 22:04:58 +0100

    Add professor enpoint to create courses

---
+++ DTOs/CourseCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EduFlow.DTOs
+{
+    public class CourseCreateDto
+    {
+        public required string Description { get; set; }
+        public required string Title { get; set; }
+    }
+}
