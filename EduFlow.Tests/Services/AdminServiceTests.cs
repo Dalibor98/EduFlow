5fa568e17a67005897ae89e8bf92f2a141aa55d5
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2026-05-27 23:31:52 +0200

    test: add AdmingService tests

---
+++ EduFlow.Tests/Services/AdminServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using EduFlow.Repositories.Interfaces;
+using EduFlow.Services.Implementations;
+using Moq;
+
+namespace EduFlow.Tests.Services
+{
+    public class AdminServiceTests
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly AdminService _adminService;
+
+        public AdminServiceTests()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _adminService = new AdminService(_userRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task RegisterProfessorAsync_EmailAlreadyExists_ThrowsArgumentException()
+        {
+            _userRepositoryMock
+                .Setup(r => r.GetByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(new Models.User() { Email = "test@example.com" });
+
+            await Assert.ThrowsAsync<ArgumentException>(
+                () => _adminService.RegisterProfessorAsync("John Doe", "test@example.com", "123")
+            );
+        }
+    }
+}
